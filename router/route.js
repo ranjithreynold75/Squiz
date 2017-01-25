647c174af1557a19fb45763a64ab68ab45fceeee
ranjith <ranjithreynold@gmail.com>
2017-01-24 20:43:18 -0800

    squizvit is online

--- router/route.js
+++ router/route.js
@@ -1,8 +1,16 @@
 var bodyparser=require('body-parser');
-//var url='mongodb://localhost:';
+//var url='mongodb://localhost:27017/squiz';
+var url="mongodb://squiz:letsdoquiz@ds056419.mlab.com:56419/squiz";
 
 module.exports=function (app) {
+
+
     app.get("/",function(req,res){
         res.send("Squiz vit is online");
     })
+
+    app.get("/",function(req,res){
+        
+    })
+
 }
