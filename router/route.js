8587fdd7b50e303fc57af6066cf496f33ed211fa
ranjith <ranjithreynold@gmail.com>
2017-02-01 21:33:30 -0800

    html updated

--- router/route.js
+++ router/route.js
@@ -173,7 +173,7 @@ quiz.insertOne(data,function(err){
 
 
 app.get("/download",function(req,res){
-    var file=__dirname+"public/image/app_icon.png";
+    var file=__dirname+"/public/image/app_icon.png";
     res.send(file);
 })
 
