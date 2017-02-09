d0ee2acde033056452ddc8e28a13c7a43f2b6696
ranjith <ranjithreynold@gmail.com>
2017-02-08 18:58:05 -0800

    commit third

--- router/route.js
+++ router/route.js
@@ -326,7 +326,7 @@ quiz.insertOne(data,function(err){
 
 app.get("/download",function(req,res){
    // console.log(__dirname);
-    var file=__dirname+"/app/app-debug-2.apk";
+    var file=__dirname+"/app/squiz.apk";
     res.download(file);
 })
 
